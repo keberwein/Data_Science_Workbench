b86d48eeac5094b76b189b4785b9c2a5981a58c4
Keberwein <kristian.eberwein@yahoo.com>
2015-11-03 19:20:01 -0500

    package test function

--- install_kernel.R
+++ install_kernel.R
@@ -1,5 +1,13 @@
-library(devtools)
-library(RCurl)
+pkgTest <- function(x)
+{
+  if (!require(x,character.only = TRUE))
+  {
+    install.packages(x,dep=TRUE)
+    if(!require(x,character.only = TRUE)) install.packages(x)
+  }
+}
+pkgTest('devtools')
+pkgTest('RCurl')
 install_github('IRkernel/repr')
 install_github('IRkernel/IRdisplay')
 install_github('IRkernel/IRkernel')
